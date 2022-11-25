d01c47c1eecfb9a07177036ab911338205a2a342
vjekas <vjekoslav.vuckovic@gmail.com>
2022-11-25 21:10:44 +0100

    Uploading Fitness website

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,10 +36,49 @@ const scrollHeader = () => {
 };
 window.addEventListener("scroll", scrollHeader);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+   const scrollY = window.pageYOffset;
+
+   sections.forEach((current) => {
+      const sectionHeight = current.offsetHeight,
+         sectionTop = current.offsetTop - 58,
+         sectionId = current.getAttribute("id"),
+         sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+         sectionsClass.classList.add("active-link");
+      } else {
+         sectionsClass.classList.remove("active-link");
+      }
+   });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+   const scrollUp = document.getElementById("scroll-up");
+   /* when scroll is higher then 350 viewport height, add show-scroll class */
+   this.scrollY >= 350
+      ? scrollUp.classList.add("show-scroll")
+      : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+   origin: "top",
+   distance: "60px",
+   duration: 2500,
+   delay: 400,
+});
+
+sr.reveal(`.home__data, .footer__container, .footer__group`);
+sr.reveal(`.home__img`, { delay: 700, origin: "bottom" });
+sr.reveal(`.logos__img, .program__card, .pricing__card`, { interval: 100 });
+sr.reveal(`.choose__img, .calculate__content`, { origin: "left" });
+sr.reveal(`.choose__content, .calculate__img`, { origin: "right" });
 
 /*=============== CALCULATE JS ===============*/
 const calculateForm = document.getElementById("calculate-form");
@@ -87,10 +126,57 @@ const calculateBmi = (e) => {
 
       setTimeout(() => {
          calculateMessage.textContent = "";
-      }, 4000);
+      }, 5000);
    }
 };
 
 calculateForm.addEventListener("submit", calculateBmi);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form");
+contactMessage = document.getElementById("contact-message");
+contactUser = document.getElementById("contact-user");
+
+const sendEmail = (e) => {
+   e.preventDefault();
+
+   /* check if field had a value */
+   if (contactUser.value === "") {
+      /* add and remove color */
+      contactMessage.classList.remove("color-green");
+      contactMessage.classList.add("color-red");
+
+      /* show message */
+      contactMessage.textContent = "Unesi email adresu";
+
+      /* remove message 3 sec */
+      setTimeout(() => {
+         contactMessage.textContent = "";
+      }, 3000);
+   } else {
+      /* serviceID - templateID - #form - publicKey */
+      emailjs
+         .sendForm("service_rohomez", "template_z73nxpo", "#contact-form", "TjiRiH4MvjQQ6J_Fe")
+         .then(
+            () => {
+               /* show message and add color */
+               contactMessage.classList.add("color-green");
+               contactMessage.textContent = "Uspešno ste se registrovali";
+
+               /* remove message 3 sec */
+               setTimeout(() => {
+                  contactMessage.textContent = "";
+               }, 3000);
+            },
+            (error) => {
+               /* mail sending error */
+               alert("UPS! DOŠLO JE DO GREŠKE...", error);
+            }
+         );
+
+      /* mail sending error */
+      contactUser.value = "";
+   }
+};
+
+contactForm.addEventListener("submit", sendEmail);
